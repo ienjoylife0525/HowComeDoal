ee882194b8059d1d07ca14074fda5fed92ee7eb5
Patato <ienjoylife0525@gmail.com>
2018-12-11 12:21:16 +0800

    Create project

---
+++ HowComeDoal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowComeDoal
+//
+//  Created by Patato on 2018/12/11.
+//  Copyright © 2018 Patato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
